723d1f42da62d83a855c476890c3c549347ae843
kate alvarez <77174960+katevalvarez@users.noreply.github.com>
2026-08-16 16:42:56 +0800

    add map

--- main.js
+++ main.js
@@ -2,10 +2,43 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
-// Follow this pattern to import other Firebase services
-// import { } from 'firebase/<service>';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Import Google Maps
+(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+key: "AIzaSyCoCXtc1E_jk_DrT-ypXjszx_ObmKPW7JM",
+v: "weekly",
+});
+
+/*
+* Google Map
+*/
+let map;
+let currentCenter;
+let centerLat;
+let centerLng;
+async function init() {
+    // Import the needed libraries
+    const { Map } = await google.maps.importLibrary('maps');
+
+    // Create a new map from the div with id="map".
+    map = new Map(document.getElementById('google-map'), {
+        center: { lat: -34.397, lng: 150.644 },
+        zoom: 8,
+    });
+
+    console.log(map);
+
+    currentCenter = map.getCenter();
+    centerLat = currentCenter.lat();
+    centerLng = currentCenter.lng();
+
+    map.addListener("center_changed", () => {
+        currentCenter = map.getCenter();
+        centerLat = currentCenter.lat();
+        centerLng = currentCenter.lng();
+        console.log(`New Lat: ${centerLat}, New Lng: ${centerLng}`);
+    });
+}
+void init();
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -24,28 +57,31 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
-// Function to generate a random coordinate
-function getRandomCoordinate(min, max) {
-    return (Math.random() * (max - min) + min).toFixed(6);
-}
-
-// Generate random Lat (-90 to 90) and Lng (-180 to 180)
-const randomLat = parseFloat(getRandomCoordinate(-90, 90));
-const randomLng = parseFloat(getRandomCoordinate(-180, 180));
+// Generate lat and long
+const dateString = new Date().toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric'
+});
 
 // Reference to 'clicks' node in the database
 const clicksRef = ref(db, 'clicks');
+const button = document.getElementById('myButton');
 
-// Push the random coordinates to Firebase
-push(clicksRef, {
-    lat: randomLat,
-    lng: randomLng,
-    timestamp: Date.now()
-}).then(() => {
-    // Update HTML text once the write succeeds
-    const statusHeading = document.getElementById('status');
-    statusHeading.textContent = `I placed lat (${randomLat}) and lng (${randomLng}) into the database`;
-}).catch((error) => {
-    console.error("Error writing to database: ", error);
-    document.getElementById('status').textContent = "Failed to write data. Check console for details.";
+// Uploads coords to database
+button.addEventListener('click', () => {
+  // Push the random coordinates to Firebase
+  push(clicksRef, {
+      lat: centerLat,
+      lng: centerLng,
+      timestamp: dateString
+  }).then(() => {
+      // Update HTML text once the write succeeds
+      const statusHeading = document.getElementById('status');
+      statusHeading.textContent = `I placed lat (${centerLat}) and lng (${centerLng}) into the database`;
+  }).catch((error) => {
+      console.error("Error writing to database: ", error);
+      document.getElementById('status').textContent = "Failed to write data. Check console for details.";
+  });
 });
+
